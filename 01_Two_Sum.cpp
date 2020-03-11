51194db5381a3a28c5d503090402253f6b4f90bc
george16886 <george16886@gmail.com>
2020-03-11 21:26:25 +0800

    #01 done

--- 01_Two_Sum.cpp
+++ 01_Two_Sum.cpp
@@ -3,38 +3,35 @@
 #include <vector>
 using namespace std;
 
-class Solution {
-public:
-
+class Solution1 {
+   public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int idx1, idx2;
-        vector<int> v(2);
-        
+        for (int i = 0; i < nums.size(); i++)
+            for (int j = i + 1; j < nums.size(); j++)
+                if ((nums[i] + nums[j]) == target)
+                    return {i, j};
+        return {};
+    }
+};
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hmap;
         for (int i = 0; i < nums.size(); i++) {
-            int ans = target - nums[i];
-            idx1 = i;
-            for (int j = 1 + i; j < nums.size(); j++) {
-                ans = ans - nums[j];
-                idx2 = j;
-                if (ans == 0) {
-                    v[0] = idx1;
-                    v[1] = idx2;
-                    return v;
-                }
-                ans = ans + nums[j];
-            }
+            if (hmap.count(target - nums[i]))
+                return {hmap[target - nums[i]], i};
+            hmap[nums[i]] = i;
         }
+        return {};
     }
 };
 
 int main(int argc, char** argv) {
     Solution solution;
-    vector<int> nums;
-    nums.push_back(2);
-    nums.push_back(7);
-    nums.push_back(11);
-    nums.push_back(15);
-    int target = 9;
+    // vector<int> nums = {2, 7, 11, 15};
+    // int target = 9;
+    vector<int> nums = {3, 2, 3};
+    int target = 6;
     vector<int> ans = solution.twoSum(nums, target);
     cout << "The answer is: [" << ans[0] << ", " << ans[1] << "]." << endl;
     return 0;
