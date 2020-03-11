e589d76a82875a48848b82bf24447b38c66298ae
george16886 <george16886@gmail.com>
2020-03-11 12:44:02 +0800

    ENV setup

--- 01_Two_Sum.cpp
+++ 01_Two_Sum.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -20,3 +24,17 @@ public:
         }
     }
 };
+
+int main(int argc, char **argv)
+{
+  Solution solution;
+  vector<int> nums;
+	nums.push_back(2);
+	nums.push_back(7);
+	nums.push_back(11);
+	nums.push_back(15);
+  int target = 9;
+  vector<int> ans = solution.twoSum(nums, target);  
+  cout << "The answer is: [" << ans[0] << ", " << ans[1] << "]." << endl;
+  return 0;
+}
