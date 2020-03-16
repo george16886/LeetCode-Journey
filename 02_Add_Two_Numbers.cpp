0917428fcc6658483f8217e5bfead4ea80f46f77
george16886 <george16886@gmail.com>
2020-03-16 15:59:46 +0800

    #02 done

--- 02_Add_Two_Numbers.cpp
+++ 02_Add_Two_Numbers.cpp
@@ -1,41 +1,102 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
+#include <stddef.h>
+#include <iostream>
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution1 {
+   public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int first = l1->val + l2->val;
-        int carry = 0;
-        if (first > 9) {
-                first = first - 10;
-                carry = 1;
-            } 
-        ListNode* firstNode = new ListNode(first);
-        ListNode* ans = firstNode;
-        while (l1->next != 0 || l2->next != 0) {
-            if (l1->next != 0) l1 = l1->next;
-            else l1->val = 0;
-            if (l2->next != 0) l2 = l2->next; 
-            else l2->val = 0;
-            ListNode* newNode = new ListNode(l1->val + l2-> val + carry);
-            firstNode->next = newNode;
-            carry = 0;
-            if (newNode->val > 9) {
-                newNode->val = newNode->val - 10;
-                carry = 1;
-            } 
-            firstNode = firstNode->next;
+        ListNode* ans = new ListNode(-1);
+        ListNode* cur = ans;
+        int c = 0;
+
+        while (l1 || l2) {
+            int a = (l1) ? l1->val : 0;
+            int b = (l2) ? l2->val : 0;
+            int sum = a + b + c;
+            c = sum / 10;
+            cur->next = new ListNode(sum % 10);
+            cur = cur->next;
+            l1 = (l1) ? l1->next : l1;
+            l2 = (l2) ? l2->next : l2;
         }
-        if (carry == 1) {
-            ListNode* newNode = new ListNode(1);
-            firstNode->next = newNode;
-            firstNode = firstNode->next;
-        }
+
+        if (c) cur->next = new ListNode(1);
+
+        return ans->next;
+    }
+};
+
+class Solution {
+   public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int c = 0;
+        ListNode* ans = addABC(l1, l2, c);
+
+        return ans;
+    }
+
+    ListNode* addABC(ListNode* l1, ListNode* l2, int& c) {
+        int a = (l1) ? l1->val : 0;
+        int b = (l2) ? l2->val : 0;
+        int sum = a + b + c;
+        ListNode* ans = new ListNode(sum % 10);
+
+        if (!l1 && !l2)
+            if (c)
+                return ans;
+            else
+                return NULL;
+
+        l1 = (l1) ? l1->next : l1;
+        l2 = (l2) ? l2->next : l2;
+        c = sum / 10;
+        ans->next = addABC(l1, l2, c);
+
         return ans;
     }
 };
+
+// Function to print a linked list
+void printList(ListNode* node) {
+    std::cout << "(";
+    while (node) {
+        std::cout << node->val;
+        if (node->next != NULL)
+            std::cout << " -> ";
+        node = node->next;
+    }
+    std::cout << ")";
+}
+
+int main(int argc, char** argv) {
+    printf("Input: ");
+
+    ListNode* a1 = new ListNode(2);
+    ListNode* a2 = new ListNode(4);
+    ListNode* a3 = new ListNode(3);
+    a1->next = a2;
+    a2->next = a3;
+    printList(a1);
+
+    printf(" + ");
+
+    ListNode* b1 = new ListNode(5);
+    ListNode* b2 = new ListNode(6);
+    ListNode* b3 = new ListNode(4);
+    b1->next = b2;
+    b2->next = b3;
+    printList(b1);
+
+    Solution solution;
+    ListNode* ans = solution.addTwoNumbers(a1, b1);
+
+    printf("\nOutput: ");
+    printList(ans);
+    printf("\n");
+    return 0;
+}
