dedba98758660e7f52416b2fefbd0509bf0ae32c
george16886 <george16886@gmail.com>
2020-03-18 14:59:20 +0800

    #05 solution2

--- 05_Longest_Palindromic_Substring.cpp
+++ 05_Longest_Palindromic_Substring.cpp
@@ -2,19 +2,17 @@
 #include <string>
 using namespace std;
 
-class Solution {
+class Solution1 {
    public:
     string longestPalindrome(string s) {
         int max_length = 1;
         int start_idx = 0;
 
         for (int i = 1; i < s.length(); i++) {
-            int left = i, right = i;
+            checkPalindrome(s, i, i, start_idx, max_length);
 
-            checkPalindrome(s, left, right, start_idx, max_length);
-
-            if (s[i - 1] == s[i])
-                checkPalindrome(s, left - 1, right, start_idx, max_length);
+            if (s[i] == s[i - 1])
+                checkPalindrome(s, i - 1, i, start_idx, max_length);
         }
 
         return s.substr(start_idx, max_length);
@@ -30,6 +28,33 @@ class Solution {
         if (length > max_length) {
             max_length = length;
             start_idx = left;
+        }
+    }
+};
+class Solution {
+   public:
+    string longestPalindrome(string s) {
+        int max_length = 1;
+        int start_idx = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            checkPalindrome(s, i, i, start_idx, max_length);
+            checkPalindrome(s, i, i + 1, start_idx, max_length);
+        }
+
+        return s.substr(start_idx, max_length);
+    }
+
+    void checkPalindrome(string s, int left, int right, int& start_idx, int& max_length) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+
+        int length = right - left + 1 - 2;
+        if (length > max_length) {
+            max_length = length;
+            start_idx = left + 1;
         }
     }
 };
