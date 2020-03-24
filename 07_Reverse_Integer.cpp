df6fca89ffb34d5b47873c56696ae67a35e7afe4
george16886 <george16886@gmail.com>
2020-03-24 10:52:30 +0800

    Today is my birthday!

--- 07_Reverse_Integer.cpp
+++ 07_Reverse_Integer.cpp
@@ -1,11 +1,14 @@
+#include <iostream>
+
 class Solution {
-public:
+   public:
     int reverse(int x) {
         bool neg = false;
         if (x < 0) {
             x = -x;
             neg = true;
         }
+
         int div = x;
         int ans = 0;
         while (div > 0) {
@@ -13,6 +16,28 @@ public:
             ans = ans * 10 + div % 10;
             div = div / 10;
         }
+
         return (neg) ? -ans : ans;
     }
 };
+
+int main(int argc, char** argv) {
+    Solution solution;
+
+    int a = 123;
+    int ans = solution.reverse(a);
+    std::cout << "Input: " << a << std::endl;
+    std::cout << "Output: " << ans << std::endl;
+
+    a = -123;
+    ans = solution.reverse(a);
+    std::cout << "Input: " << a << std::endl;
+    std::cout << "Output: " << ans << std::endl;
+
+    a = 120;
+    ans = solution.reverse(a);
+    std::cout << "Input: " << a << std::endl;
+    std::cout << "Output: " << ans << std::endl;
+
+    return 0;
+}
