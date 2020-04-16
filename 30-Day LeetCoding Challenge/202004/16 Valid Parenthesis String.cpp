148da52ce830f50486b987abef14a5f44debda92
george16886 <george16886@gmail.com>
2020-04-17 01:29:01 +0800

    #20200416 solution1

--- 30-Day LeetCoding Challenge/202004/16 Valid Parenthesis String.cpp
+++ 30-Day LeetCoding Challenge/202004/16 Valid Parenthesis String.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 class Solution {
    public:
     bool checkValidString(string s) {
-        
-        return true;
+        stack<int> left;
+        stack<int> star;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(')
+                left.push(i);
+            else if (s[i] == '*')
+                star.push(i);
+            else if (s[i] == ')') {
+                if (left.empty() && star.empty())
+                    return false;
+                else if (!left.empty())
+                    left.pop();
+                else if (!star.empty())
+                    star.pop();
+            }
+        }
+
+        while (!left.empty() && !star.empty()) {
+            if (left.top() > star.top())
+                return false;
+            else {
+                left.pop();
+                star.pop();
+            }
+        }
+
+        return left.empty();
     }
 };
 
